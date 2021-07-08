68a6d633955b25a9116d75cd0befe6cee5df0b3f
Chetan-Chinchulkar <chetanchinchulkar.1802@gmail.com>
2021-07-08 20:39:19 +0530

    Initial Files

--- SAIL_SITE/events/models.py
+++ SAIL_SITE/events/models.py
@@ -3,21 +3,3 @@ from django.db import models
 from django.urls import reverse
 
 # Create your models here.
-
-class Event(models.Model):
-    title  = models.CharField(max_length=50)
-    # Specify location for 'default' and 'upload_to'
-    bg_image = models.ImageField(default='',upload_to='',Null=True)
-    yt_link = models.URLField(default="",max_length=300,Null=True)
-    content = models.TextField()
-
-    def save(self,*args, **kwargs):
-        super().save(*args, **kwargs)
-
-        img = Image.open(self.bg_image.path)
-
-        #   Dimensions are hardcoded, relative method required
-        if img.height > 450 or img.width > 350:
-            output_size = (450, 350)
-            img.thumbnail(output_size)
-            img.save(self.bg_image.path)
--- SAIL_SITE/events/urls.py
+++ SAIL_SITE/events/urls.py
@@ -1,13 +1,8 @@
 from django.urls import path, reverse_lazy
 from . import views
-from .views import EventListView,EventDetailView, event
+
 
 app_name='events'
 urlpatterns = [
-    path('events/',EventListView.as_view(),name='event-home'),
-    path('events/<int:pk>/', EventDetailView.as_view(), name='events-detail'),
-
     
-    
-
 ]
