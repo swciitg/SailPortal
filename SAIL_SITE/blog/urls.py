ff680eea35d471f8a6a2c2f6c2ba7d0a41057730
Chetan-Chinchulkar <chetanchinchulkar.1802@gmail.com>
2021-07-23 13:32:00 +0530

    Update urls.py

--- SAIL_SITE/blog/urls.py
+++ SAIL_SITE/blog/urls.py
@@ -7,6 +7,6 @@ app_name='blog'
 urlpatterns = [
     path('', BlogListView.as_view(),name='blog-home'),
     
-    #path('post/<int:pk>/', BlogDetailView.as_view(), name = 'blog-detail'),
+    path('<int:pk>/', BlogDetailView.as_view(), name = 'blog-detail'),
     
 ]
