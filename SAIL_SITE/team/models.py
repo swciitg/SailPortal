6c984645ba1f6ffea51618bd752312d05518fc14
Shreya Goel <shreya.goel@iitg.ac.in>
2021-10-26 02:31:57 +0530

    Added teams page

--- SAIL_SITE/team/models.py
+++ SAIL_SITE/team/models.py
@@ -1,8 +1,9 @@
 from django.db import models
-
+from datetime import datetime
 
 # Create your models here.
 class Position(models.Model):
+    rank = models.PositiveSmallIntegerField(null= True, blank= True, default = 1)
     name = models.CharField(max_length=50)
   
 
@@ -21,12 +22,15 @@ class Category(models.Model):
 class Team(models.Model):
     name = models.CharField(max_length=50)
     position = models.ForeignKey(Position, on_delete=models.CASCADE)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    # category = models.ForeignKey(Category, on_delete=models.CASCADE)
     email = models.EmailField(max_length=254)
     facebook = models.URLField(max_length=200,null=True,blank=True)
     linkeden = models.URLField(max_length=200,null=True,blank=True)
-    pic = models.ImageField(upload_to='team', height_field=None, width_field=None, max_length=None,default="")
-
+    # pic = models.ImageField(upload_to='team', height_field=None, width_field=None, max_length=None,default="")
+    pic = models.URLField(max_length=1000)
+    is_alumnus = models.BooleanField(default = True)
+    year = models.PositiveSmallIntegerField(default = datetime.now().year)
+    pic_id = models.CharField(blank= True, null = True, max_length = 50)
 
     def __str__(self):
         return self.name
--- SAIL_SITE/team/views.py
+++ SAIL_SITE/team/views.py
@@ -2,8 +2,14 @@ from django.shortcuts import render
 from .models import Team
 # Create your views here.
 from django.views.generic import ListView
+from django.db.models import Q
 
+def PastExecutives(request):
+    memberlist =  Team.objects.filter(Q(is_alumnus=True), Q(position__name ="General Secretary")).order_by("-year")
+    print(memberlist)
+    return render(request, "team/pastexec.html", {"memberList": memberlist})
 
-class TeamListView(ListView):
-    model = Team
-    template_name = "team/team_list.html"
+def TeamListView(request):
+    team =  Team.objects.filter(Q(is_alumnus=False)).order_by("position__rank")
+    print(team)
+    return render(request, "team/team_list.html", {"team": team})
