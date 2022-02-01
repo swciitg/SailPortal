b8704795cc94f484ec8436b6cd44336482204427
Chetan Chinchulkar <76626599+Chetan-Chinchulkar@users.noreply.github.com>
2022-02-01 14:58:39 +0530

    content added + minor changes

--- SAIL_SITE/initiatives/models.py
+++ SAIL_SITE/initiatives/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from ckeditor.fields import RichTextField
 
 class upload(models.Model):
-    title=models.CharField(max_length=20)
+    title=models.CharField(max_length=50)
     image=models.ImageField(upload_to="img/%y")
     desc=RichTextField(blank=True,null=True)
     url=models.URLField(blank=True, max_length=200)
@@ -10,3 +10,8 @@ class upload(models.Model):
 
     def __str__(self):
         return self.title
+
+
+
+
+        
